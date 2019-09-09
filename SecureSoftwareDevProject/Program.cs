4d2aefd32e4ab41330e56496a079aacd36303cbd
S00171657 <S00171657@mail.itsligo.ie>
2019-09-09 18:00:54 +0100

    Project Start

---
+++ SecureSoftwareDevProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SecureSoftwareDevProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
